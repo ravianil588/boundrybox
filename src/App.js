58dea202f121ff3022aae33abd5749358bc9a824
ravianil588 <59193840+ravianil588@users.noreply.github.com>
2019-12-24 15:23:27 +0530

    Update App.js
    removed comments

--- src/App.js
+++ src/App.js
@@ -24,28 +24,6 @@ class App extends Component {
       }
     };
   }
-  // onImageChange = event => {
-  //   this.setState({
-  //     showImage: false
-  //   });
-  //   if (event.target.files && event.target.files[0]) {
-  //     let reader = new FileReader();
-  //     reader.onload = e => {
-  //       this.setState({ image: e.target.result });
-  //     };
-  //     reader.readAsDataURL(event.target.files[0]);
-  //   }
-  // };
-  // handleCoordinates = event => {
-  //   let boxes = this.state.boxes;
-
-  //   boxes[parseInt(event.target.name)] = event.target.value;
-
-  //   this.setState({
-  //     boxes
-  //   });
-  //   //console.log(boxes);
-  // };
   handleCLick = () => {
     this.setState({
       showImages: false
@@ -76,53 +54,6 @@ class App extends Component {
       ))}
       </div>
     </div> : '' }
-        
-
-        {/* <div className="inputImage">
-          <span>
-            <input
-              type="file"
-              onChange={this.onImageChange}
-              className="filetype"
-              id="single"
-            />
-          </span>
-        </div>
-        <div className="firstCord">
-          {" "}
-          X1 :{" "}
-          <input type="number" name="0" onChange={this.handleCoordinates} />
-          Y1 :{" "}
-          <input
-            type="number"
-            name="1"
-            onChange={this.handleCoordinates}
-          />{" "}
-        </div>
-        <div className="secondCord">
-          {" "}
-          X2 :{" "}
-          <input type="number" name="2" onChange={this.handleCoordinates} />
-          Y2 :{" "}
-          <input
-            type="number"
-            name="3"
-            onChange={this.handleCoordinates}
-          />{" "}
-        </div>
-
-        <div>
-          <button
-            type="button"
-            className="btn btn-primary submitButton"
-            onClick={this.handleCLick}
-          >
-            Generate i  mage
-          </button>
-        </div> */}
-        {/* <div className="customImage">
-          {this.state.showImage ? <BoundingBox {...this.state} /> : ""}
-        </div> */}
       </div>
     );
   }
